5f234cf1dc55d1535a64d59963c0da44292aa718
geraldvindas <gerald.vindas@ucr.ac.cr>
2019-06-15 21:14:54 -0600

    Se cambia a proyecto tipo scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainSearchEngineComponent } from './modules/main-search-engine/main-search-engine.component';
-import { SecondarySearcherComponent } from './modules/secondary-searcher/secondary-searcher.component';
-import { SearchResultsComponent } from './modules/search-results/search-results.component';
-import {RouterModule} from '@angular/router';
-import {AppRoutingModule} from './app-routing.module';
-import {SearchResultsRoutingModule} from './modules/search-results/search-results-routing.module';
-import { ResultComponent } from './modules/result/result.component';
-import {ResultRoutingModule} from './modules/result/result-routing.module';
+import {MainSearchEngineComponent} from './modules/main-search-engine/main-search-engine.component';
+import {SecondarySearcherComponent} from './modules/secondary-searcher/secondary-searcher.component';
+import {SearchResultsComponent} from './modules/search-results/search-results.component';
+import {ResultComponent} from './modules/result/result.component';
 import {MainSearchEngineModule} from './modules/main-search-engine/main-search-engine.module';
+import {SearchResultsRoutingModule} from './modules/search-results/search-results-routing.module';
+import {ResultRoutingModule} from './modules/result/result-routing.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
--- src/app/modules/secondary-searcher/secondary-searcher.component.ts
+++ src/app/modules/secondary-searcher/secondary-searcher.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-secondary-searcher',
   templateUrl: './secondary-searcher.component.html',
-  styleUrls: ['./secondary-searcher.component.css']
+  styleUrls: ['./secondary-searcher.component.scss']
 })
 export class SecondarySearcherComponent implements OnInit {
 
